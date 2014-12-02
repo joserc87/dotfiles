41af3d5770e9582e4648f5803b514ad28679c220
Jose Ramon Cano <joserc87@gmail.com>
2014-12-02 22:16:44 +0100

    Initializing the repository. Created the directory tree with some files.

---
+++ scripts/generateXresources.sh
@@ -0,0 +1,1 @@
+TERM=rxvt-unicode-256color command man -Pcat urxvt | sed -n '/depth: b/,/^BA/p'|sed '$d'|sed '/^       [a-z]/s/^ */^/g'|sed -e :a -e 'N;s/\n/@@/g;ta;P;D'|sed 's,\^\([^@]\+\)@*[\t ]*\([^\^]\+\),! \2\n! URxvt*\1\n\n,g'|sed 's,@@\(  \+\),\n\1,g'|sed 's,@*$,,g'|sed '/^[^!]/d'|tr -d "'\`" >> ~/.XresourcesTemplate
