96238cb4241d8dda009d4b1d9733d0eedde47662
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2016-10-12 20:59:14 +0200

    Update installation scripts.
    install.sh calls MacOSX/programs.sh to install programs. Also, Vundle is
    cloned only if not present.
    
    Add installation of oh my zsh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-git clone https://github.com/VundleVim/Vundle.vim.git $/.vim/bundle/Vundle.vim
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Install software
+./MacOSX/programs.sh
+# Link dot files
+./dotfiles/symlink.sh
+# Vundle
+if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
+    echo "Installing Vundle";
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+else
+    echo "Vundle already installed";
+fi
