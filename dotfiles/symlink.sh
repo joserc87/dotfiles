8df88e718940431250b8deb1a36e6d0518c60ac0
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2016-10-12 19:58:19 +0200

    Move the git config files into dotfiles
    Changed some git configurations

--- dotfiles/symlink.sh
+++ dotfiles/symlink.sh
@@ -5,14 +5,31 @@ symlink_home () {
 		dir=`pwd`/
 		cd ~
 		echo "Creating symbolic link for ~/"$2
-		echo ln -s $dir$1$2 $2
+		# echo ln -s $dir$1$2 $2
 		ln -s $dir$1$2 $2
 		cd $dir
 	fi
 }
 
+symlink() {
+	if [ ! -e $3$4 ]; then
+		dir=`pwd`/
+		cd $3
+		echo "Creating symbolic link for "$3$4
+		# echo ln -s $dir$1$2 $4
+		ln -s $dir$1$2 $4
+		cd $dir
+	fi
+}
+
+
+
 symlink_home vim/ .vim
 symlink_home vim/ .vimrc
+
+symlink_home git/ .gitconfig
+symlink_home git/ .gitignore_global
+
 symlink_home "" .bash_profile
 symlink_home "" .zshrc
 symlink_home "" .tmux.conf
