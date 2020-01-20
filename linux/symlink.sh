fffdaf29012dce019e5c939728eb2fce1f19287e
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2020-01-21 00:04:55 +0100

    Symlink .config linux files

--- linux/symlink.sh
+++ linux/symlink.sh
@@ -6,10 +6,10 @@ dotfiles=""
 
 # Link script files
 warn "Linking linux config files\n";
-symlink linux/.config clipit ~/.config clipit 
-symlink linux/.config i3 ~/.config i3 
-symlink linux/.config i3blocks ~/.config i3blocks 
-symlink linux/.config i3lock ~/.config i3lock 
-symlink linux/.config ranger ~/.config ranger 
-symlink linux/.config rofi ~/.config rofi 
-symlink linux/.config compton.conf ~/.config comton.conf 
+symlink .config/ clipit ~/.config clipit 
+symlink .config/ i3 ~/.config i3 
+symlink .config/ i3blocks ~/.config i3blocks 
+symlink .config/ i3lock ~/.config i3lock 
+symlink .config/ ranger ~/.config ranger 
+symlink .config/ rofi ~/.config rofi 
+symlink .config/ compton.conf ~/.config compton.conf 
