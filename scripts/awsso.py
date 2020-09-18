71956171988929834769240f30658de83addc42a
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2020-09-18 18:49:17 +0200

    Fix a few scripts

--- scripts/awsso.py
+++ scripts/awsso.py
@@ -1,8 +1,9 @@
-from os import path
+from os import path, environ
 
+HOME = environ.get('HOME', '/home/jose')
 CREDENTIALS_PATH = path.join(
     # path.expanduser('~'),
-    '/home/jose',
+    HOME,
     '.aws/credentials'
 )
 
