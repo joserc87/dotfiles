ea2f3e5a4e01a05f73cda8d92f70c38a1eea62a3
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2021-10-15 22:56:52 +0200

    Fix qtile config crash

--- linux/.config/qtile/lib/layouts.py
+++ linux/.config/qtile/lib/layouts.py
@@ -3,6 +3,7 @@ Layout definitions
 """
 from libqtile import layout
 from .settings import COLS
+from libqtile.config import Match
 
 _layout_common_settings = dict(
     border_focus=COLS['purple_4']
@@ -15,9 +16,9 @@ _max_layout_settings = {
 
 # Layouts
 floating_layout = layout.Floating(float_rules=[
-    dict(wmclass='float'),
-    dict(wmclass='floating'),
-    dict(wmclass="zoom"),
+    Match(wm_class='float'),
+    Match(wm_class='floating'),
+    Match(wm_class="zoom"),
 ])
 layouts = [
     layout.MonadTall(name='GapsBig', **_layout_common_settings, margin=192),
