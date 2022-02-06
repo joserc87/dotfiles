a265468d364ab318e612583d5f0026634da7246d
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2022-02-06 13:40:15 +0100

    Tweak script

--- scripts/toggle_lamp.sh
+++ scripts/toggle_lamp.sh
@@ -11,14 +11,17 @@ lamp_on=$(curl $API/lights/$DESK_LAMP | jq .state.on)
 BRIGHTNESS=${1:-255}
 
 if [[ $lamp_on == 'true' ]]; then
-    if [[ -z "$1" ]]; then
+    if [[ -z "$1" ]] || [[ $1 == '0' ]] || [[ $1 == 'off' ]]; then
         echo "The lamp is on. Turning off."
         curl -X PUT "$API/lights/$DESK_LAMP/state" -d '{"on":false}'
     else
         echo "The lamp is on. Adjusting."
         curl -X PUT "$API/lights/$DESK_LAMP/state" -d "{\"on\":true,\"bri\":$BRIGHTNESS}"
     fi
-else
+elif [[ $1 != 'off' ]]; then
+    if [[ $1 == 'on' ]]; then
+        BRIGHTNESS=255
+    fi
     echo "The lamp is off. Turning on."
     curl -X PUT "$API/lights/$DESK_LAMP/state" -d "{\"on\":true,\"bri\":$BRIGHTNESS}"
 fi
