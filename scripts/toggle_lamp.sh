460ca35d766e362e12d584bfe504649e594a0475
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2022-08-11 15:42:42 +0200

    Add custom scripts

--- scripts/toggle_lamp.sh
+++ scripts/toggle_lamp.sh
@@ -6,22 +6,22 @@ API="$ADDR/api/$USR"
 DESK_LAMP=6
 
 STATE_URL=$API/lights/1
-lamp_on=$(curl $API/lights/$DESK_LAMP | jq .state.on)
+lamp_on=$(curl --connect-timeout 2 $API/lights/$DESK_LAMP | jq .state.on)
 
 BRIGHTNESS=${1:-255}
 
 if [[ $lamp_on == 'true' ]]; then
     if [[ -z "$1" ]] || [[ $1 == '0' ]] || [[ $1 == 'off' ]]; then
         echo "The lamp is on. Turning off."
-        curl -X PUT "$API/lights/$DESK_LAMP/state" -d '{"on":false}'
+        curl -X PUT --connect-timeout 2 "$API/lights/$DESK_LAMP/state" -d '{"on":false}'
     else
         echo "The lamp is on. Adjusting."
-        curl -X PUT "$API/lights/$DESK_LAMP/state" -d "{\"on\":true,\"bri\":$BRIGHTNESS}"
+        curl -X PUT --connect-timeout 2 "$API/lights/$DESK_LAMP/state" -d "{\"on\":true,\"bri\":$BRIGHTNESS}"
     fi
 elif [[ $1 != 'off' ]]; then
     if [[ $1 == 'on' ]]; then
         BRIGHTNESS=255
     fi
     echo "The lamp is off. Turning on."
-    curl -X PUT "$API/lights/$DESK_LAMP/state" -d "{\"on\":true,\"bri\":$BRIGHTNESS}"
+    curl -X PUT --connect-timeout 2 "$API/lights/$DESK_LAMP/state" -d "{\"on\":true,\"bri\":$BRIGHTNESS}"
 fi
