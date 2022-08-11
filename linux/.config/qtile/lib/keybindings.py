b31ba429117580b6fee642d9eaa634025e5e23d8
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2022-08-11 15:36:11 +0200

    Qtile config

--- linux/.config/qtile/lib/keybindings.py
+++ linux/.config/qtile/lib/keybindings.py
@@ -1,93 +1,117 @@
 from libqtile.config import Key
 from lib.keys import (
-    ALT, TAB, CTRL, SHIFT, RETURN, SPACE, GRAVE,
-    DELETE, PRINT, BACKSPACE, SEMICOLON, COMMA, ESCAPE
+    ALT,
+    TAB,
+    CTRL,
+    SHIFT,
+    RETURN,
+    SPACE,
+    GRAVE,
+    DELETE,
+    PRINT,
+    BACKSPACE,
+    SEMICOLON,
+    COMMA,
+    ESCAPE,
 )
 from libqtile.command import lazy
-from lib.settings import (
-    MODKEY, DEFAULT_TERMINAL, APP_LAUNCHER,
-    DEFAULT_TERMINAL
-)
+from lib.settings import MODKEY, DEFAULT_TERMINAL, APP_LAUNCHER, DEFAULT_TERMINAL
 from lib.utils import (
-    exe, term_exe, kick_to_next_screen, go_to_next_screen,
-    go_to_group_or_switch_screen, next_layout, toggle_gaps
+    exe,
+    term_exe,
+    kick_to_next_screen,
+    go_to_next_screen,
+    go_to_group_or_switch_screen,
+    next_layout,
+    increase_gaps,
+    decrease_gaps,
 )
 from lib.groups import groups
 
 # Keybindings
 keys = [
-
     # General
-    Key([MODKEY, SHIFT], 'r', lazy.restart()),
+    Key([MODKEY, SHIFT], "r", lazy.restart()),
     Key([MODKEY], RETURN, exe(DEFAULT_TERMINAL)),
     Key([MODKEY], BACKSPACE, exe(DEFAULT_TERMINAL)),
-    Key([MODKEY], 'e', exe('dmenu_run')),
-    Key([MODKEY], 'z', exe('morc_menu')),
-    Key([MODKEY, SHIFT], 'q', lazy.window.kill()),
-    Key([MODKEY, CTRL], 'q', lazy.window.kill()),
-    Key([MODKEY], 'F5', term_exe('mocp')),
-    Key([MODKEY], DELETE, exe('logoff')),
-    Key([MODKEY], 'c', exe('compton-toggle')),
-    Key([MODKEY, SHIFT], 'e', exe('sgtk-bar')),
+    Key([MODKEY], "e", exe("dmenu_run")),
+    Key([MODKEY], "z", exe("morc_menu")),
+    Key([MODKEY, SHIFT], "q", lazy.window.kill()),
+    Key([MODKEY, CTRL], "q", lazy.window.kill()),
+    Key([MODKEY], "F5", term_exe("mocp")),
+    Key([MODKEY], DELETE, exe("logoff")),
+    Key([MODKEY, SHIFT], "c", exe("compton-toggle")),
+    Key([MODKEY, SHIFT], "e", exe("sgtk-bar")),
     Key([MODKEY, SHIFT], ESCAPE, lazy.shutdown()),
-
     # TODO: Add Screenshots like i3-scrot
     Key([MODKEY], PRINT, lazy.spawn(["scrot", "-s"])),
-
     # Move Focus
     Key([ALT], TAB, lazy.layout.next()),
     Key([ALT, SHIFT], TAB, lazy.layout.previous()),
-    Key([MODKEY], 'h', lazy.layout.left()),
-    Key([MODKEY], 'j', lazy.layout.down()),
-    Key([MODKEY], 'k', lazy.layout.up()),
-    Key([MODKEY], 'l', lazy.layout.right()),
-    #Key([MODKEY, SHIFT], SPACE, lazy.layout.flip()),
-
+    Key([MODKEY], "h", lazy.layout.left()),
+    Key([MODKEY], "j", lazy.layout.down()),
+    Key([MODKEY], "k", lazy.layout.up()),
+    Key([MODKEY], "l", lazy.layout.right()),
+    # Key([MODKEY, SHIFT], SPACE, lazy.layout.flip()),
     # Move Window
-    Key([MODKEY, SHIFT], 'j', lazy.layout.shuffle_down()),
-    Key([MODKEY, SHIFT], 'k', lazy.layout.shuffle_up()),
-    Key([MODKEY, SHIFT], 'h', lazy.layout.shuffle_left()),
-    Key([MODKEY, SHIFT], 'l', lazy.layout.shuffle_right()),
-    Key([MODKEY, CTRL], 'j', lazy.layout.shuffle_down()),
-    Key([MODKEY, CTRL], 'k', lazy.layout.shuffle_up()),
-    Key([MODKEY, CTRL], 'h', lazy.layout.shuffle_left()),
-    Key([MODKEY, CTRL], 'l', lazy.layout.shuffle_right()),
+    Key([MODKEY, SHIFT], "j", lazy.layout.shuffle_down()),
+    Key([MODKEY, SHIFT], "k", lazy.layout.shuffle_up()),
+    Key([MODKEY, SHIFT], "h", lazy.layout.shuffle_left()),
+    Key([MODKEY, SHIFT], "l", lazy.layout.shuffle_right()),
+    Key([MODKEY, CTRL], "j", lazy.layout.shuffle_down()),
+    Key([MODKEY, CTRL], "k", lazy.layout.shuffle_up()),
+    Key([MODKEY, CTRL], "h", lazy.layout.shuffle_left()),
+    Key([MODKEY, CTRL], "l", lazy.layout.shuffle_right()),
     Key([MODKEY], COMMA, lazy.layout.swap_main()),
-
-
     # Alter Window Size
-    Key([MODKEY], 'minus', lazy.layout.shrink()),
-    Key([MODKEY], 'equal', lazy.layout.grow()),
-    Key([MODKEY, SHIFT], 'equal', lazy.layout.reset()),
-    Key([MODKEY, CTRL], 'equal', lazy.layout.reset()),
-    Key([MODKEY], 'm', lazy.layout.maximize()),
-    Key([MODKEY], 'n', lazy.layout.normalize()),
-
+    Key([MODKEY], "minus", lazy.layout.shrink()),
+    Key([MODKEY], "equal", lazy.layout.grow()),
+    Key([MODKEY, SHIFT], "equal", lazy.layout.reset()),
+    Key([MODKEY, CTRL], "equal", lazy.layout.reset()),
+    Key([MODKEY], "m", lazy.layout.maximize()),
+    Key([MODKEY], "n", lazy.layout.normalize()),
     # Layout
     Key([MODKEY, CTRL], SPACE, lazy.window.toggle_floating()),
-    Key([MODKEY, SHIFT], SPACE, exe('switchkblayout')),
+    Key([MODKEY, SHIFT], SPACE, exe("switchkblayout")),
     Key([MODKEY], SPACE, next_layout()),
-    Key([MODKEY], 'r', lazy.window.toggle_fullscreen()),
-
+    Key([MODKEY], "r", lazy.window.toggle_fullscreen()),
     # Screens
     # Key([MODKEY], 'i', lazy.function(go_to_next_screen, -1)),
     # Key([MODKEY], 'o', lazy.function(go_to_next_screen)),
-    Key([MODKEY], 'q', toggle_gaps()),
-    Key([MODKEY], 'w', toggle_gaps()),
+    Key([MODKEY], "q", increase_gaps()),
+    Key([MODKEY], "w", decrease_gaps()),
     Key([MODKEY], SEMICOLON, lazy.function(go_to_next_screen)),
-    Key([MODKEY, CTRL], 'h', lazy.function(kick_to_next_screen, -1)),
-    Key([MODKEY, CTRL], 'l', lazy.function(kick_to_next_screen)),
-    Key([], 'F12', lazy.group['scratchpad'].dropdown_toggle('diary')),
-    Key([SHIFT], 'F12', lazy.group['scratchpad'].dropdown_toggle('task')),
+    Key([MODKEY], "x", lazy.to_screen(0)),
+    Key([MODKEY], "c", lazy.to_screen(1)),
+    Key([MODKEY], "v", lazy.to_screen(2)),
+    Key([MODKEY, CTRL], "h", lazy.function(kick_to_next_screen, -1)),
+    Key([MODKEY, CTRL], "l", lazy.function(kick_to_next_screen)),
+    Key([], "F12", lazy.group["scratchpad"].dropdown_toggle("diary")),
+    Key([CTRL], "F12", lazy.group["scratchpad"].dropdown_toggle("task")),
+
+    # Multimedia
+
+    # Volume
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -D pulse sset Master 2%+")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -D pulse sset Master 2%-")),
+    Key([], "XF86AudioMute", lazy.spawn("amixer -D pulse sset Master toggle")),
+
+    # Brightness
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +5%")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 5%-")),
+    Key([], "XF86TouchpadToggle", lazy.spawn("i3lock-fancy")),
+
+    Key([MODKEY], "Print", lazy.spawn("flameshot gui")),
+    Key([MODKEY, SHIFT], "Print", lazy.spawn("flameshot launcher")),
 ]
 
 # Keybindings for switching groups
-for g, k in zip(groups, [GRAVE] + list('asdfgyuiop')):
-    # keys.append(Key([MODKEY], k, lazy.group[g.name].toscreen()))
-    keys.append(Key([MODKEY], k, lazy.function(go_to_group_or_switch_screen, g.name)))
+for g, k in zip(groups, [GRAVE] + list("asdfgyuiop")):
+    keys.append(Key([MODKEY], k, lazy.group[g.name].toscreen()))
+    # keys.append(Key([MODKEY], k, lazy.function(go_to_group_or_switch_screen, g.name)))
     keys.append(Key([MODKEY, SHIFT], k, lazy.window.togroup(g.name)))
     keys.append(Key([MODKEY, CTRL], k, lazy.window.togroup(g.name)))
-    
+
 
 def modifier_window_commands(match, spawn, *keys):
     # Use switch_window by default (just mod)
@@ -98,9 +122,6 @@ def modifier_window_commands(match, spawn, *keys):
         ([mod], switch_window(**match)),
         ([mod, "control"], pull_window_here(**match)),
         ([mod, "shift"], lazy.spawn(spawn)),
-        ([mod, "shift", "control"], pull_window_group_here(**match)))
-    return [Key(mods, key, command)
-            for mods, command in mapping
-            for key in keys]
-
-
+        ([mod, "shift", "control"], pull_window_group_here(**match)),
+    )
+    return [Key(mods, key, command) for mods, command in mapping for key in keys]
--- linux/.config/qtile/lib/layouts.py
+++ linux/.config/qtile/lib/layouts.py
@@ -8,6 +8,7 @@ from libqtile.config import Match
 _layout_common_settings = dict(
     border_focus=COLS['purple_4'],
     border_normal=COLS['dark_1'],
+    border_width=0,
     single_border_width=0,
 )
 
@@ -16,6 +17,12 @@ _max_layout_settings = {
     "border_focus": None
 }
 
+_main_satelite = {
+    **_layout_common_settings,
+    "ratio": 0.7,
+}
+
+
 # Layouts
 floating_layout = layout.Floating(float_rules=[
     Match(wm_class='float'),
@@ -23,8 +30,10 @@ floating_layout = layout.Floating(float_rules=[
     Match(wm_class="zoom"),
 ])
 layouts = [
+    layout.MonadTall(name='MainSatelite', **_main_satelite, margin=48),
     layout.MonadTall(name='GapsBig', **_layout_common_settings, margin=192),
     layout.MonadTall(name='GapsSmall', **_layout_common_settings, margin=48),
+    layout.MonadTall(name='NoGaps', **_layout_common_settings, margin=1),
     # layout.Floating(**_layout_common_settings),
     # layout.VerticalTile(name='VerticalTile'),
     layout.Max(name='Full', **_layout_common_settings),
