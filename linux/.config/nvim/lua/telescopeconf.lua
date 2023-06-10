1e8f661e88ae267c86aac017e9843ea0fa0e667a
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2023-06-10 17:36:27 +0200

    Ignore node_modules in telescope

--- linux/.config/nvim/lua/telescopeconf.lua
+++ linux/.config/nvim/lua/telescopeconf.lua
@@ -18,6 +18,7 @@
 local actions = require('telescope.actions')
 require('telescope').setup {
   defaults = {
+    file_ignore_patterns = {"node_modules/.*", ".git/.*"},
     mappings = {
       i = {
         ['<C-u>'] = false,
