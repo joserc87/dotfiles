e7b64acb2a45da6ed8738aac2d21874f24291b9d
Jose Ramon Cano <jcano@ravenpack.com>
2024-01-03 13:10:22 +0100

    Archive old scripts

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-# Define a timestamp function
-timestamp() {
-  date +"%Y-%m-%d_%H-%M-%S.%3N"
-}
-
-echo "L $(timestamp)" >> ~/lock.log
-echo "U $(timestamp)" >> ~/lock.log
-echo "L $(timestamp)" >> ~/lock.log
-echo "U $(timestamp)" >> ~/lock.log
