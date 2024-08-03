e4a14ee31489268c777641afc444711245e44d97
Jose Ramon Cano <joserc87@gmail.com>
2024-08-03 14:45:20 +0200

    Mount as user

--- scripts/mountnas.sh
+++ scripts/mountnas.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-sudo mount -t cifs -o username=jose //192.168.100.205/photos /mnt/photos
-sudo mount -t cifs -o username=jose //192.168.100.205/jose /mnt/jose
+uid=$(id -u)
+gid=$(id -g)
+
+sudo mount -t cifs -o username=jose,uid=$uid,gid=$gid //192.168.100.205/photos /mnt/photos
+sudo mount -t cifs -o username=jose,uid=$uid,gid=$gid //192.168.100.205/jose /mnt/jose
