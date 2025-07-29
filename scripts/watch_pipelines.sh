e3fa8ddf4ea59d041b50f9e0d45a9426eff6d6cb
Jose Ramon Cano <joserc87@gmail.com>
2025-07-29 18:31:23 +0200

    Update script

--- scripts/watch_pipelines.sh
+++ scripts/watch_pipelines.sh
@@ -3,4 +3,12 @@
 export DIR=$HOME/git/python/smart-topics
 
 cd $DIR
-last_id=$(glab ci list -u jcano5 -s running --source push -F json -P 1 | jq '.[0].id')
+pipeline_url=""
+while [ -z "$pipeline_url" ]; do
+  pipeline_url=$(glab ci list -u jcano5 -s running --source push -F json -P 1 | jq -r '.[0].web_url')
+  if [ -z "$pipeline_url" ]; then
+    echo -n "."
+    sleep 10
+  fi
+done
+xdg-open "$pipeline_url"
